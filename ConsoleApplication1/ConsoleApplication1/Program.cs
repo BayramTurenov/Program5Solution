3deb55d9e244e1b1ee69c7e91e76d697eede593c
BayramTurenov <bturenov@gmail.com>
2014-10-28 21:54:50 -0600

    FunctionPractise

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+
+            int myAgeDoubled = DoubleIt(34);
+            Console.WriteLine(DoubleIt(myAgeDoubled));
+        }
+
+        /// <summary>
+        /// Writes "Hello world" to the console
+        /// </summary>
+        static void HelloWorld()
+        {
+            Console.WriteLine("Hello world");
+            
+        }
+        static int DoubleIt(int someNumber)
+        {
+            return someNumber * 2;
+        }
+        /// <summary>
+        ///Prints numbers to the console from start to the end 
+        /// </summary>
+        /// <param name="startNumber"></param>
+        /// <param name="endNumber"></param>
+        static void LoopSomeNumbers(int startNumber, int endNumber)
+        {
+            for(int i = startNumber; i<=endNumber; i = i + 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        /// <summary>
+        /// Runs a series of numbers
+        /// </summary>
+        static void LoopTests()
+        {
+            LoopSomeNumbers(5,10);
+            LoopSomeNumbers(863428,863430);
+            LoopSomeNumbers(31,DoubleIt(DoubleIt(31)));
+        }
+
+        
+
+
+        /// <summary>
+        /// String that of vowels in a string
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        static int VowelCounter3000(string inputString) 
+        {
+            //declares a counter fro the vowels
+            int numberOfVowelsFound = 0;
+            //loop over each letter of the string
+            for (int i = 0; i < inputString.Length; i = i + 1)
+            {
+                string letter = inputString[i].ToString().ToLower();
+                if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+                {
+                    //
+                    numberOfVowelsFound = numberOfVowelsFound + 1;
+                    //alternative way of checking to see if its a vowel
+                    if ("aeiou".Contains(letter)) { }
+
+                }
+                //loop complete, time to write the output
+                Console.WriteLine(inputString + " has" + numberOfVowelsFound + " vowels in it");
+                return numberOfVowelsFound;
+            }
+        }
+
+            static void VowelCounter3000Tests()
+            {
+            int totalNumberVowelCounted = 0;
+                totalNumberVowelCounted += VowelCounter3000("bla bla");
+                totalNumberVowelCounted += VowelCounter3000("I eat bla bla");
+                Console.WriteLine("Total is VowelCounter3000");
+            }
+
+        }
+    }
+
---
+++ LoopingOf/LoopingOf/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopOverAList();
+            LoopOverWordsInAString("the quick brown fox jump over lazzy dog");
+            Console.ReadKey();
+        }
+        static void LoopOverAList()
+        {
+            //create list of sports
+            List<string> sportList = new List<string>() { "Baseball", "Tennis" };
+            //add Football to the sportList
+            sportList.Add("Football");
+            //loop over the sport list and display all elements that contain the world "ball"
+            for (int i = 0; i < sportList.Count(); i++)
+            {
+            //get the current sport out of sportlist
+                string currentSport = sportList[i];
+                //check to see if its a sport contains "ball"
+                if (currentSport.Contains("ball"))
+                {
+                    //its true
+                    Console.WriteLine(currentSport);
+                }
+            }
+        }
+        static void LoopOverWordsInAString(string inputString)
+        {
+            List<string> wordList = inputString.Split(' ').ToList();
+            for (int i = 0; i < wordList.Count(); i = i + 1)
+            {
+                Console.WriteLine(wordList[i]);
+            }
+        }
+     }
+}
